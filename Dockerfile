ee37d05eeca96d3b11afb6dff13514292d011bb6
douglasacost <douglasacostaaquino@gmail.com>
2022-11-02 01:23:38 -0400

    add script for change dynamically the network values

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,14 @@ FROM onfinality/subql-node:latest
 
 COPY ./ /app
 
-RUN apk add --no-cache python3 py3-pip make g++\
+RUN apk add --no-cache bash python3 py3-pip make g++\
     && rm -rf /var/cache/apk/*
 RUN cd app\
     && yarn install\
     && yarn codegen && yarn build
 
-CMD ["-f=/app", "--db-schema=app", "--log-level=debug", "--disable-historical=false", "--progress=plain"]
+RUN chmod +x ./app/scripts/update_project_file.sh
+
+CMD ["-f=/app", "--db-schema=app", "--log-level=error", "--disable-historical=false"]
+
+ENTRYPOINT ["./app/scripts/update_project_file.sh"]
---
+++ scripts/update_project_file.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+filename="./app/project.yaml"
+
+# Check if file exists
+if [ -f "$filename" ]
+then
+    echo "$filename found."
+else
+    echo "$filename not found."
+    exit 1
+fi
+
+ENVS="$ENVS NETWORK_CHAIN_ID"
+ENVS="$ENVS NETWORK_ENDPOINT"
+
+VALUES="$VALUES ${NETWORK_CHAIN_ID}"
+VALUES="$VALUES ${NETWORK_ENDPOINT}"
+
+# Replace the values in the file
+for i in $(seq 1 $(echo "$ENVS" | wc -w)); do
+    index=$((i+1))
+    env=$(echo "$ENVS" | cut -d ' ' -f $index)
+    value=$(echo "$VALUES" | cut -d ' ' -f $index)
+    if [ $value ]
+    then
+        echo "Replacing $env with $value"
+        sed -i "s~$env~$value~g" $filename
+    else
+        echo "Value for $env is empty"
+    fi
+done
