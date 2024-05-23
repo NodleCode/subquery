c14c6369e198392e33b811294b8657167b0426fd
douglasacost <douglasacostaaquino@gmail.com>
2024-05-23 23:06:25 +0200

    chore: Remove unused code and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import "@polkadot/types-augment";
 import "@polkadot/api-augment";
 
-export * from "./mappings/mappingSponsorshipHandlers";
 export * from "./mappings/mappingUniquesHandlers";
-export * from "./mappings/mappingBalancesHandlers";
